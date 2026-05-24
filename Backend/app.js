e6caf3c956eca341f4d2a8db117ab3065a2f76d7
coder-nik200 <nitishkb1707@gmail.com>
2026-05-24 12:47:31 +0530

    Remove Frontend url from backend

--- Backend/app.js
+++ Backend/app.js
@@ -21,7 +21,6 @@ const taskAssignmentRoute = require("./routes/taskAssignmentRoute");
 // CORS
 app.use(
   cors({
-    // origin: "http://localhost:5173",
     origin: process.env.FRONTEND_URL,
     credentials: true,
   }),
